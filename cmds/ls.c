f7ce3c8876b8638bcd6a461c48179f1528feda2d
Ashwitha Yadav T <aswithayadav@gmail.com>
2017-10-20 19:21:25 +0530

    Added code for -a option for ls (display hidden files and directories also).

--- cmds/ls.c
+++ cmds/ls.c
@@ -7,20 +7,32 @@
 int main(int argc, char ** argv){
 
 	DIR * dir;
-	struct dirent * entry;
 
-	// Open the directory stream
-	if ( argv[1] == NULL ){		// List current directory
-		if ( (dir = opendir(".")) == NULL ){
-			perror("minsh");
-			return 0;
-		}
+	// An array of directory entries would make it a lot easier to apply the options
+	// I'm assuming that no directory has more than 1024 entries!
+	struct dirent * entry[1024];
+	char * directory;
+	char option;
+
+	// By default, the second argument will be the directory
+	directory = argv[1];
+
+	// Check if an option is specified
+	if ( argv[1] != NULL && argv[1][0] == '-'  ){	// Option is specified
+		option = argv[1][1];
+		// If option is specified, the third argument is the directory
+		directory = argv[2];
 	}
-	else{
-		if ( (dir = opendir(argv[1])) == NULL ){
-			perror("minsh");
-			return 0;
-		}
+
+	// Check if directory is specified
+	if ( directory == NULL ){
+		directory = ".";
+	}
+
+	// Open directory stream
+	if ( (dir = opendir(directory)) == NULL ){
+		perror("minsh");
+		return 0;
 	}
 
 	// Read every directory entry and print it
@@ -28,22 +40,36 @@ int main(int argc, char ** argv){
 	errno = 0;	// Distinguish between end of directory
 			// and error
 
+	int entno = 0;	// Entry number - keep track of position in the array of entries
 	while ( 1 ){
-		entry = readdir(dir);
-		if (entry == NULL){
+		// Adds the entry to the array of entries
+		// Automatically adds NULL to specify the end too!!
+		entry[entno] = readdir(dir);
+		if (entry[entno] == NULL){
 			if (errno == 0){	// End of directory
-				printf("\n\n");
-				return 0;
+				break;
 			}
 			else{	// Error has occurred
 				perror("minsh");
 				return 1;
 			}
 		}
-		else{	// Entry available
-			printf("\n%s",entry->d_name);
+		entno++;
+	}
+	
+	// Now apply the options
+	for (entno = 0 ; entry[entno] != NULL ; entno++){
+		if (entry[entno]->d_name[0] == '.'){	// Hidden file or directory 
+			if (option == 'a'){	// Option is 'a' (for hidden)
+				printf("\n%s", entry[entno]->d_name);
+			}
+		}
+		else{
+			printf("\n%s", entry[entno]->d_name);
 		}
 	}
+	printf("\n\n");
+
 	return 0;
 }
 
